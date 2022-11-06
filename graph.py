48bc3f8aa22f49790bc679de9a1782b02d71d83b
이도현 <idohyeon@idohyeon-ui-MacBookPro.local>
2022-11-06 23:35:29 +0900

    simple_push

--- graph.py
+++ graph.py
@@ -22,21 +22,19 @@ line = [line1, line2, line3, line4, line5, line6]
 def animate(i):
     # axis limits checking. Same as before, just for both axes
     data =pd.read_csv('/home/dohlee/crc_project/data/data1.csv')
-    x_value = data['x_value']
     gyro_x = data['gyro_x']
     gyro_y = data['gyro_y']
     gyro_z = data['gyro_z']
     acc_x = data['acc_x']
     acc_y = data['acc_y']
     acc_z = data['acc_z']
-    
-    gx.plot(x_value, gyro_x, lw =2)
-    gy.plot(x_value, gyro_y, lw =2) 
-    gz.plot(x_value, gyro_z, lw =2) 
-    ax.plot(x_value, acc_x, lw =2) 
-    ay.plot(x_value, acc_y, lw =2) 
-    az.plot(x_value, acc_z, lw =2)
-    print(x_value)
+
+    gx.plot(0, gyro_x, lw =2)
+    gy.plot(0, gyro_y, lw =2) 
+    gz.plot(0, gyro_z, lw =2) 
+    ax.plot(0, acc_x, lw =2) 
+    ay.plot(0, acc_y, lw =2) 
+    az.plot(0, acc_z, lw =2)
     
     #line[0].set_data(x_value, gyro_x)
     #line[1].set_data(x_value, gyro_y)
