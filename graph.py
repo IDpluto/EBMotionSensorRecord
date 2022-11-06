1920391ef3c4cd526574679e7f6d2376bc6d4eee
이도현 <idohyeon@idohyeon-ui-MacBookPro.local>
2022-11-06 22:15:07 +0900

    simple_push

--- graph.py
+++ graph.py
@@ -8,12 +8,12 @@ from pandas.core.indexes import interval
 f, (gx, gy, gz, ax, ay, az) = plt.subplots(6, 3)
 f.set_size_inches((10, 5))
 plt.subplots_adjust(wspace = 0.3, hspace = 0.3)
-line1, = gx.plot([], [], lw =2)
-line2, = gy.plot([], [], lw =2) 
-line3, = gz.plot([], [], lw =2) 
-line4, = ax.plot([], [], lw =2) 
-line5, = ay.plot([], [], lw =2) 
-line6, = az.plot([], [], lw =2)
+line1, = gx.plot([0], [0], lw =2)
+line2, = gy.plot([0], [0], lw =2) 
+line3, = gz.plot([0], [0], lw =2) 
+line4, = ax.plot([0], [0], lw =2) 
+line5, = ay.plot([0], [0], lw =2) 
+line6, = az.plot([0], [0], lw =2)
 line = [line1, line2, line3, line4, line5, line6]
 
  
