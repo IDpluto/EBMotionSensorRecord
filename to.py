2dff3938d9643e9156dd56da9fff8492fa19cc99
이도현 <idohyeon@idohyeon-ui-MacBookPro.local>
2022-11-07 00:37:47 +0900

    simple_push

--- to.py
+++ to.py
@@ -6,10 +6,10 @@ import numpy as np
 arduino = serial.Serial('/dev/ttyUSB0', 921600)
 
 fig = plt.figure()
-ax = plt.axes(xlim=(0, 50), ylim=(-100, 100))
+ax = plt.axes(xlim=(0, 10), ylim=(-10, 10))
 line, = ax.plot([], [], lw=2)
 
-max_points = 50
+max_points = 10
 line, = ax.plot(np.arange(max_points), 
                 np.ones(max_points, dtype=np.float)*np.nan, lw=2)
 
@@ -28,6 +28,6 @@ def animate(i):
     line.set_ydata(new_y)
     return line,
 
-anim = animation.FuncAnimation(fig, animate, init_func=init, frames=200, interval=20, blit=False)
+anim = animation.FuncAnimation(fig, animate, init_func=init, frames=200, interval=10, blit=False)
 
 plt.show()
