61d25cc45c35d7651b9092805679fdf6323a7e25
이도현 <idohyeon@idohyeon-ui-MacBookPro.local>
2022-11-07 01:31:19 +0900

    simple_push

--- real_time.py
+++ real_time.py
@@ -4,20 +4,19 @@ import signal
 from itertools import count
 import pandas as pd
 import numpy as np
-from matplotlib import pyplot as plt
-from matplotlib import animation
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from pandas.core.indexes import interval
 
 ser = serial.Serial('/dev/ttyUSB0', 921600)
 fig, (gx, ax) = plt.subplots(2,1)
-
+fig.set_size_inches((10, 5))
+fig.subplots_adjust(wspace = 0.9, hspace = 0.9)
 line1, = gx.plot([], [], lw=2)
 line2, = ax.plot([], [], lw=2, color='r')
 line = [line1, line2]
-fig.set_size_inches((10, 5))
-fig.subplots_adjust(wspace = 0.9, hspace = 0.9)
 
-def init():
-    return line1, line2,
+
 
 def animate(i):
     tmp = ser.readline()
@@ -33,7 +32,7 @@ def animate(i):
     #line[1].set_data(gyro_y)
     
 
-ani = animation.FuncAnimation(fig , animate, blit=False, frames= 200, interval = 100)
+ani = FuncAnimation(fig , animate, blit=False, frames= 200, interval = 100)
 plt.show()
     
 
