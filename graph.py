52a2f34fbd727384b0e8ac1ec526d92011927899
이도현 <idohyeon@idohyeon-ui-MacBookPro.local>
2022-11-06 23:38:22 +0900

    simple_push

--- graph.py
+++ graph.py
@@ -11,12 +11,12 @@ fig, (gx, gy, gz, ax, ay, az) = plt.subplots(6,1)
 fig.set_size_inches((10, 5))
 fig.subplots_adjust(wspace = 0.9, hspace = 0.9)
 
-line1, = gx.plot([], [], lw =2)
-line2, = gy.plot([], [], lw =2) 
-line3, = gz.plot([], [], lw =2) 
-line4, = ax.plot([], [], lw =2) 
-line5, = ay.plot([], [], lw =2) 
-line6, = az.plot([], [], lw =2)
+line1, = gx.plot([], lw =2)
+line2, = gy.plot([], lw =2) 
+line3, = gz.plot([], lw =2) 
+line4, = ax.plot([], lw =2) 
+line5, = ay.plot([], lw =2) 
+line6, = az.plot([], lw =2)
 line = [line1, line2, line3, line4, line5, line6]
 
 def animate(i):
@@ -29,19 +29,13 @@ def animate(i):
     acc_y = data['acc_y']
     acc_z = data['acc_z']
 
-    gx.plot(gyro_x, lw =2)
-    gy.plot(gyro_y, lw =2) 
-    gz.plot(gyro_z, lw =2) 
-    ax.plot(acc_x, lw =2) 
-    ay.plot(acc_y, lw =2) 
-    az.plot(acc_z, lw =2)
     
-    #line[0].set_data(x_value, gyro_x)
-    #line[1].set_data(x_value, gyro_y)
-    #line[2].set_data(x_value, gyro_z)
-    #line[3].set_data(x_value, acc_x)
-    #line[4].set_data(x_value, acc_y)
-    #line[5].set_data(x_value, acc_z)
+    line[0].set_data(gyro_x)
+    line[1].set_data(gyro_y)
+    line[2].set_data(gyro_z)
+    line[3].set_data(acc_x)
+    line[4].set_data(acc_y)
+    line[5].set_data(acc_z)
     
     return line
 
