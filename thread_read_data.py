2c348832c30643961b3ea8b9e41c0d434757239d
이도현 <idohyeon@idohyeon-ui-MacBookPro.local>
2022-11-06 18:56:47 +0900

    simple_push

--- thread_read_data.py
+++ thread_read_data.py
@@ -2,11 +2,7 @@ import serial
 import time
 import signal
 import threading
-from itertools import count
-import pandas as pd
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
-from pandas.core.indexes import interval
+
 
 line = [] #라인 단위로 데이터 가져올 리스트 변수
 
@@ -14,6 +10,7 @@ port = '/dev/ttyUSB0' # 시리얼 포트
 baud = 921600 # 시리얼 보드레이트(통신속도)
 
 exitThread = False   # 쓰레드 종료용 변수
+
 
 #쓰레드 종료용 시그널 함수
 def handler(signum, frame):
@@ -26,26 +23,8 @@ def parsing_data(data):
     # 작업하기 편하게 스트링으로 합침
     tmp = ''.join(data)
     tmp = tmp.split(',')
-    #data =pd.read_csv('data4.csv')
-    girox = data[1]
-    giroy = data[2]
-    giroz = data[3]
-    accx = data[4]
-    accy = data[5]
-    accz = data[6]
- 
-    plt.cla()
-    plt.plot(girox, label = 'Giro_x')
-    plt.plot(giroy, label = 'Giro_y')
-    plt.plot(giroz, label = 'Giro_z')
-    plt.plot(accx, label = 'Acc_x')
-    plt.plot(accy, label = 'Acc_y')
-    plt.plot(accz, label = 'Acc_z')
-
-    
-    
-    plt.legend(loc = 'upper left')
-    plt.tight_layout()
+    #출력!
+    print(tmp[0])
 
 #본 쓰레드
 def readThread(ser):
@@ -78,6 +57,3 @@ if __name__ == "__main__":
 
     #시작!
     thread.start()
-    ani = FuncAnimation(plt.gcf(),animate, interval = 1000)
-    plt.tight_layout()
-    plt.show()
