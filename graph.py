ebf8de823c4d9f2b1e0b34f67aea7b1fdbbf0e46
이도현 <idohyeon@idohyeon-ui-MacBookPro.local>
2022-11-06 20:33:27 +0900

    simple_push

--- graph.py
+++ graph.py
@@ -1,13 +1,14 @@
-import random
 from itertools import count
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from pandas.core.indexes import interval
  
- 
+f, axes = plt.subplot(0,2)
+f.set_size_inches((20, 15))
  
 def animate(i):
+    
     data =pd.read_csv('/home/dohlee/crc_project/data/data1.csv')
     gyro_x = data['gyro_x']
     gyro_y = data['gyro_y']
@@ -16,16 +17,28 @@ def animate(i):
     acc_y = data['acc_y']
     acc_z = data['acc_z']
  
+    #plt.cla()
+    #plt.plot(gyro_x, label='Gyro_x')
+    #plt.plot(gyro_y, label='Gyro_y')
+    #plt.plot(gyro_z, label='Gyro_z')
+    #plt.plot(acc_x, label='acc_x')
+    #plt.plot(acc_y, label='acc_y')
+    #plt.plot(acc_z, label='acc_z')
+    #plt.legend(loc = 'upper left')
+    #plt.tight_layout()
+
     plt.cla()
-    plt.plot(gyro_x, label='Gyro_x')
-    plt.plot(gyro_y, label='Gyro_y')
-    plt.plot(gyro_z, label='Gyro_z')
-    plt.plot(acc_x, label='acc_x')
-    plt.plot(acc_y, label='acc_y')
-    plt.plot(acc_z, label='acc_z')
+    axes[0, 1].plot(gyro_x, label='Gyro_x')
+    axes[0, 1].plot(gyro_y, label='Gyro_y')
+    axes[0, 1].plot(gyro_z, label='Gyro_z')
+    axes[0, 2].plot(acc_x, label='acc_x')
+    axes[0, 2].plot(acc_y, label='acc_y')
+    axes[0, 2].plot(acc_z, label='acc_z')
     plt.legend(loc = 'upper left')
     plt.tight_layout()
- 
+
+
+plt.subplots_adjust(wspace = 0.3, hspace = 0.3)
 ani = FuncAnimation(plt.gcf(),animate, frames= 500, interval = 50)
  
 plt.tight_layout()
