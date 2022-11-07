1cf383b267168d66364ee2cfbff4527205681ae2
이도현 <idohyeon@idohyeon-ui-MacBookPro.local>
2022-11-07 10:53:16 +0900

    simple_push

--- real_time.py
+++ real_time.py
@@ -81,6 +81,7 @@ while 1:
                 roll = float(words[data_index])*grad2rad
                 pitch = float(words[data_index+1])*grad2rad
                 yaw = float(words[data_index+2])*grad2rad
+                print(roll)
             except:
                 print (".")
         else: #(data_format==2)quaternion
