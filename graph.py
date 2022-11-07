56a422d82700af02c991dffe01e86f829d125c78
이도현 <idohyeon@idohyeon-ui-MacBookPro.local>
2022-11-08 00:25:30 +0900

    simple_push

--- graph.py
+++ graph.py
@@ -31,7 +31,7 @@ def animate(i):
    
 data =pd.read_csv('/home/dohlee/crc_project/data/data1.csv')
 sensor_id = data['sensor_id']
-if (str(sensor_id) == 'ID:100-0'):
+if (str(sensor_id) == 'ID:100-1'):
     plt.figure(figsize=(10, 5))
     ani = FuncAnimation(plt.gcf() , animate, blit=False, frames= 200, interval = 100)
     plt.show()
