aca7e7d7ffff66f47905adcb4e9078d3c4267ccf
이도현 <idohyeon@idohyeon-ui-MacBookPro.local>
2022-11-07 18:22:32 +0900

    simple_push

--- graph.py
+++ graph.py
@@ -31,7 +31,7 @@ def animate(i):
     plt.tight_layout()
    
 
-plt.figure(figsize=(5, 1))
+plt.figure(figsize=(1, 5))
 ani = FuncAnimation(plt.gcf() , animate, blit=False, frames= 200, interval = 100)
  
 plt.show()
