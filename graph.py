ce354c87166868b54b01bd673fab3a4e75758a2e
이도현 <idohyeon@idohyeon-ui-MacBookPro.local>
2022-11-07 18:17:22 +0900

    simple_push

--- graph.py
+++ graph.py
@@ -21,12 +21,12 @@ def animate(i):
     acc_z = data['acc_z']
 
     plt.cla()
-    plt.plot(x_num, roll, label = str(sensor_id), color = 'black')
-    plt.plot(x_num, pitch, label = str(sensor_id), color = 'limegreen')
-    plt.plot(x_num, yaw, label = str(sensor_id), color = 'violet')
-    plt.plot(x_num, acc_x, label = str(sensor_id), color = 'dodgerblue')
-    plt.plot(x_num, acc_y, label = str(sensor_id), color = 'red')
-    plt.plot(x_num, acc_z, label = str(sensor_id), color = 'green')
+    plt.plot(x_num, roll, label = 'roll', color = 'black')
+    plt.plot(x_num, pitch, label = 'pitch', color = 'limegreen')
+    plt.plot(x_num, yaw, label = 'yaw', color = 'violet')
+    plt.plot(x_num, acc_x, label = 'acc_x', color = 'dodgerblue')
+    plt.plot(x_num, acc_y, label = 'acc_Y', color = 'red')
+    plt.plot(x_num, acc_z, label = 'acc_z', color = 'green')
     plt.legend(loc = 'upper left')
     plt.tight_layout()
    
