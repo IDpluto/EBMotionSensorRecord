903dfd11bad67fa12d0150449238b143f1e18f5b
이도현 <idohyeon@idohyeon-ui-MacBookPro.local>
2022-11-08 22:49:35 +0900

    simple_push

--- mult_real_time_graph.py
+++ mult_real_time_graph.py
@@ -32,41 +32,54 @@ def data_gen():
     #counter = itertools.count()
     data =pd.read_csv('/home/dohlee/crc_project/data/data1.csv')
     
-    xnum = data['x_num'].astype(int)
-    roll = data['roll'].astype(float)
-    pitch = data['pitch'].astype(float)
-    yaw = data['yaw'].astype(float)
-    acc_x = data['acc_x'].astype(float)
-    acc_y = data['acc_y'].astype(float)
-    acc_z = data['acc_z'].astype(float)
-    print (xnum)
+    xnum = data['x_num']
+    roll = data['roll']
+    pitch = data['pitch']
+    yaw = data['yaw']
+    acc_x = data['acc_x']
+    acc_y = data['acc_y']
+    acc_z = data['acc_z']
     
     
 
-    yield int(xnum), float(roll), float(pitch), float(yaw), float(acc_x), float(acc_y), float(acc_z)
+    yield xnum, roll, pitch, yaw, acc_x, acc_y, acc_z
 
 
 
 
 
 def animate(data):
-    x_num, roll, pitch, yaw, acc_x, acc_y, acc_z = data
-    xdata.append(x_num)
-    r_data.append(roll)
-    p_data.append(pitch)
-    z_data.append(yaw)
-    ax_data.append(acc_x)
-    ay_data.append(acc_y)
-    az_data.append(acc_z)
+    #x_num, roll, pitch, yaw, acc_x, acc_y, acc_z = data
+    #xdata.append(x_num)
+    #r_data.append(roll)
+    #p_data.append(pitch)
+    #z_data.append(yaw)
+    #ax_data.append(acc_x)
+    #ay_data.append(acc_y)
+    #az_data.append(acc_z)
 
-    line[0].set_data(xdata, r_data)
-    line[1].set_data(xdata, p_data)
-    line[2].set_data(xdata, z_data)
-    line[3].set_data(xdata, ax_data)
-    line[4].set_data(xdata, ay_data)
-    line[5].set_data(xdata, az_data)
+    #line[0].set_data(xdata, r_data)
+    #line[1].set_data(xdata, p_data)
+    #line[2].set_data(xdata, z_data)
+    #line[3].set_data(xdata, ax_data)
+    #line[4].set_data(xdata, ay_data)
+    #line[5].set_data(xdata, az_data)
 
-    return line,
+    data =pd.read_csv('/home/dohlee/crc_project/data/data1.csv')
+    
+    xnum = data['x_num']
+    roll = data['roll']
+    pitch = data['pitch']
+    yaw = data['yaw']
+    acc_x = data['acc_x']
+    acc_y = data['acc_y']
+    acc_z = data['acc_z']
+
+    ax1.plot(xnum, roll, lw=2, color = 'red')
+
+
+
+    
 
 if __name__ == '__main__':
     
@@ -76,19 +89,19 @@ if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyUSB0', 921600)
     fig, (ax1, ax2) = plt.subplots(2,1) #, ax3, ax4) = plt.subplots(4,1)
     
-    roll_line1, = ax1.plot([],[], lw=2, color = 'red')
-    pitch_line1, = ax1.plot([], [], lw = 2, color = 'blue')
-    yaw_line1, = ax1.plot([], [], lw = 2, color = 'orange')
-    acx_line1, = ax2.plot([], [], lw = 2, color = 'red')
-    acy_line1, = ax2.plot([], [], lw = 2, color = 'blue')
-    acz_line1, = ax2.plot([], [], lw = 2, color = 'orange')
+    #roll_line1, = ax1.plot([],[], lw=2, color = 'red')
+    #pitch_line1, = ax1.plot([], [], lw = 2, color = 'blue')
+    #yaw_line1, = ax1.plot([], [], lw = 2, color = 'orange')
+    #acx_line1, = ax2.plot([], [], lw = 2, color = 'red')
+    #acy_line1, = ax2.plot([], [], lw = 2, color = 'blue')
+    #acz_line1, = ax2.plot([], [], lw = 2, color = 'orange')
     #roll_line2, = ax3.plot([], [], lw = 2, color = 'red')
     #pitch_line2, = ax3.plot([], [], lw = 2, color = 'blue')
     #yaw_line2, = ax3.plot([], [], lw = 2, color = 'orange')
     #acx_line2, = ax4.plot([], [], lw = 2, color = 'red')
     #acy_line2, = ax4.plot([], [], lw = 2, color = 'blue')
     #acz_line2, = ax4.plot([], [], lw = 2, color = 'orange')
-    line = [roll_line1, pitch_line1, yaw_line1, acx_line1, acy_line1, acz_line1]#, roll_line2, pitch_line2, yaw_line2, acx_line2, acy_line2, acz_line2]   
+    #line = [roll_line1, pitch_line1, yaw_line1, acx_line1, acy_line1, acz_line1]#, roll_line2, pitch_line2, yaw_line2, acx_line2, acy_line2, acz_line2]   
     
     ax1.set_ylim(-300, 300)
     ax1.grid()
@@ -97,7 +110,7 @@ if __name__ == '__main__':
 
     xdata, r_data, p_data, z_data, ax_data, ay_data, az_data = [], [], [], [], [], [], []
 
-    ani = animation.FuncAnimation(fig, animate, frames = data_gen, blit=False, interval=10,
+    ani = animation.FuncAnimation(plt.gcf(), animate, frames = 200, blit=False, interval=10,
         repeat=False)
     plt.show()
 
