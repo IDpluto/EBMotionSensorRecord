829af09989bd045d87dbef7ad43ffd96a0e1ebae
이도현 <idohyeon@idohyeon-ui-MacBookPro.local>
2022-11-08 22:30:15 +0900

    simple_push

--- mult_real_time_graph.py
+++ mult_real_time_graph.py
@@ -14,28 +14,108 @@ from pandas.core.indexes import interval
 
 
 
+def quat_to_euler(x,y,z,w):
+    euler = [0.0,0.0,0.0]
+    
+    sqx=x*x
+    sqy=y*y
+    sqz=z*z
+    sqw=w*w
+  
+    euler[0] = math.asin(-2.0*(x*z-y*w)) 
+    euler[1] = math.atan2(2.0*(x*y+z*w),(sqx-sqy-sqz+sqw))
+    euler[2] = math.atan2(2.0*(y*z+x*w),(-sqx-sqy+sqz+sqw)) 
+
+    return euler
+
+
+
+
+
+def data_gen():
+    #i = 0
+    counter = itertools.count()
+    while 1:
+        line = ser.readline()
+        line = line.decode("ISO-8859-1")
+        words = line.split(",")    # Fields split
+    
+        if(-1 < words[0].find('*')) :
+            data_from=1     # sensor data
+            data_index=0
+            text = "ID:"+'*'
+            words[0]=words[0].replace('*','')
+            #print ("first:", text)
+        else :
+            if(-1 < words[0].find('-')) :
+                data_from=2  # rf_receiver data
+                data_index=1
+                text = "ID:"+words[0]
+                #print ("seconds:",text)
+            else :
+                data_from=0  # unknown format
+        if(data_from!=0):
+            commoma = words[data_index].find('.') 
+            if(len(words[data_index][commoma:-1])==4): # �Ҽ��� 4�ڸ� �Ǻ�
+                data_format = 2  # quaternion
+            else:
+                data_format = 1 # euler
+
+
+            if(data_format==1): #euler
+                try:
+                    roll = float(words[data_index])*grad2rad
+                    pitch = float(words[data_index+1])*grad2rad
+                    yaw = float(words[data_index+2])*grad2rad
+                    acc_x = float(words[data_index+3])
+                    acc_y = float(words[data_index+4])
+                    acc_z = float(words[data_index+5])
+                    #print(roll)
+                except:
+                    print (".")
+            else: #(data_format==2)quaternion
+                try:
+                    q0 = float(words[data_index])
+                    q1 = float(words[data_index+1])
+                    q2 = float(words[data_index+2])
+                    q3 = float(words[data_index+3])
+                    acc_x = float(words[data_index+4])
+                    acc_y = float(words[data_index+5])
+                    acc_z = float(words[data_index+6])
+                    Euler = quat_to_euler(q0,q1,q2,q3)
+
+                    roll  = Euler[1]
+                    pitch = Euler[0]
+                    yaw   = Euler[2]
+                except:
+                    print (".")
+            t = next(counter)
+            roll_r = "%.2f" %(roll*rad2grad)
+            pitch_r = "%.2f" %(pitch*rad2grad)
+            yaw_r = "%.2f" %(yaw*rad2grad)
+    
+
+    yield t, roll_r, pitch_r, yaw_r, acc_x, acc_y, acc_z
+
+
+
 
 
 def animate(data):
-    data =pd.read_csv('/home/dohlee/crc_project/data/data1.csv')
-    
-    xnum = data['x_num'].astype(int)
-    roll = data['roll'].astype(float)
-    pitch = data['pitch'].astype(float)
-    yaw = data['yaw'].astype(float)
-    acc_x = data['acc_x'].astype(float)
-    acc_y = data['acc_y'].astype(float)
-    acc_z = data['acc_z'].astype(float)
-    #xdata.append(x_num)
-    #r_data.append(roll)
-    #p_data.append(pitch)
-    #z_data.append(yaw)
-    #ax_data.append(acc_x)
-    #ay_data.append(acc_y)
-    #az_data.append(acc_z)
-    ax1.plot(xnum, roll, lw=2, color = 'red')
-    
-   
+    x_num, roll, pitch, yaw, acc_x, acc_y, acc_z = data
+    xdata.append(x_num)
+    r_data.append(roll)
+    p_data.append(pitch)
+    z_data.append(yaw)
+    ax_data.append(acc_x)
+    ay_data.append(acc_y)
+    az_data.append(acc_z)
+    line[0].set_data(xdata, r_data)
+    line[1].set_data(xdata, p_data)
+    line[2].set_data(xdata, z_data)
+    line[3].set_data(xdata, ax_data)
+    line[4].set_data(xdata, ay_data)
+    line[5].set_data(xdata, az_data)
 
     return line,
 
@@ -68,7 +148,7 @@ if __name__ == '__main__':
 
     xdata, r_data, p_data, z_data, ax_data, ay_data, az_data = [], [], [], [], [], [], []
 
-    ani = animation.FuncAnimation(fig, animate, frames = 200, blit=False, interval=10,
+    ani = animation.FuncAnimation(fig, animate, frames = data_gen, blit=False, interval=10,
         repeat=False)
     plt.show()
 
