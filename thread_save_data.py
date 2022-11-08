4401e12f81b4f6f8811a138c783ff4412652934c
이도현 <idohyeon@idohyeon-ui-MacBookPro.local>
2022-11-08 23:30:45 +0900

    simple_push

--- thread_save_data.py
+++ thread_save_data.py
@@ -128,9 +128,9 @@ while 1:
             except:
                 print (".")
         x_count += 0.1
-        if text == "ID:100-0":
+        if text == "ID:100-1":
             save_data_hand(text, roll, pitch, yaw,acc_x, acc_y, acc_z, x_count)
-        else:
+        if text == "ID:100-1":
             save_data_head(text, roll, pitch, yaw,acc_x, acc_y, acc_z, x_count)
 
    
