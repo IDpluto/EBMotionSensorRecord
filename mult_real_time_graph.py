98bf27b5f9ef2e1e057e4e8850747462e121e836
이도현 <idohyeon@idohyeon-ui-MacBookPro.local>
2022-11-08 22:44:08 +0900

    simple_push

--- mult_real_time_graph.py
+++ mult_real_time_graph.py
@@ -43,7 +43,7 @@ def data_gen():
     
     
 
-    yield xnum, roll, pitch, yaw, acc_x, acc_y, acc_z
+    yield int(xnum), float(roll), float(pitch), float(yaw), float(acc_x), float(acc_y), float(acc_z)
 
 
 
