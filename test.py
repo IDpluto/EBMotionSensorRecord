39bead1ce1e20e8c28517adb592e33b177eb01aa
이도현 <idohyeon@idohyeon-ui-MacBookPro.local>
2022-11-09 20:22:53 +0900

    simple_push

--- test.py
+++ test.py
@@ -3,21 +3,18 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 fig, ax = plt.subplots()
+line, = ax.plot(np.random.rand(10))
+ax.set_ylim(0, 1)
 
-x = np.arange(0, 2*np.pi, 0.01)
-line, = ax.plot(x, np.sin(x))
 
-
-def animate(i):
-    line.set_ydata(np.sin(x + i/10.0))  # update the data
+def update(data):
+    line.set_ydata(data)
     return line,
 
 
-# Init only required for blitting to give a clean slate.
-def init():
-    line.set_ydata(np.ma.array(x, mask=True))
-    return line,
+def data_gen():
+    while True:
+        yield np.random.rand(10)
 
-ani = animation.FuncAnimation(fig, animate, np.arange(1, 200), init_func=init,
-                              interval=25, blit=True)
+ani = animation.FuncAnimation(fig, update, data_gen, interval=100)
 plt.show()
