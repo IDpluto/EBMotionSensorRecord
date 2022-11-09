a50b5d6fdc328ca9a7a88404a34e5b6d03276895
이도현 <idohyeon@idohyeon-ui-MacBookPro.local>
2022-11-09 20:08:53 +0900

    simple_push

--- mult_real_time_graph.py
+++ mult_real_time_graph.py
@@ -74,8 +74,11 @@ if __name__ == '__main__':
     ax2.set_ylim(-3, 3)
     ax2.grid()
 
-    ani = animation.FuncAnimation(fig, animate, frames = 500, blit=True, interval=10,
-        init_func=init, repeat=False)
+    #ani = animation.FuncAnimation(fig, animate, frames = 500, blit=True, interval=10,
+    #    init_func=init, repeat=False)
+    ani = animation.FuncAnimation(fig, animate, np.arange(1, 200), init_func=init,
+                              interval=25, blit=True)
+
     plt.show()
 
     
