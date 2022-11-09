2323696a1f8b153dadc8f7883c481cc4d17c7c3e
이도현 <idohyeon@idohyeon-ui-MacBookPro.local>
2022-11-09 19:23:15 +0900

    simple_push

--- simple_graph.py
+++ simple_graph.py
@@ -17,7 +17,8 @@ def animate(data):
     
     
     sensor = data['sensor_id']
-    if (sensor == '1'):
+
+    if (sensor == True):
         xnum = data['x_num']
         roll1 = data['roll']
         pitch1 = data['pitch']
@@ -26,7 +27,7 @@ def animate(data):
         acc_y1 = data['acc_y']
         acc_z1 = data['acc_z']
     else:
-        if (sensor == '0'):
+        if (sensor == False):
             xnum = data['x_num']
             roll2 = data['roll']
             pitch2 = data['pitch']
