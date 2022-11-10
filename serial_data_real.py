6af33a7241545cea89ec2266215e1cab43d25229
이도현 <idohyeon@idohyeon-ui-MacBookPro.local>
2022-11-11 07:54:53 +0900

    simple_push

--- serial_data_real.py
+++ serial_data_real.py
@@ -70,8 +70,8 @@ fig = plt.figure()
 ax = plt.subplot(211, xlim=(0, 5), ylim=(-500, 500))
 ax_2 = plt.subplot(212, xlim=(0, 5), ylim=(-3, 3))
 
-max_points = 5000
-max_points_2 = 5000
+max_points = 50
+max_points_2 = 50
 
 line, = ax.plot(np.arange(max_points), 
                 np.ones(max_points, dtype=np.float64)*np.nan, lw=1, c='blue',ms=1)
