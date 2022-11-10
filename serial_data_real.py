38bcdcc5b06ec2a398cf718dd71bf0873e34b3a5
이도현 <idohyeon@idohyeon-ui-MacBookPro.local>
2022-11-11 05:13:20 +0900

    simple_push

--- serial_data_real.py
+++ serial_data_real.py
@@ -1,4 +1,4 @@
-import RPi.GPIO as GPIO
+import serial
 from matplotlib import pyplot as plt
 from matplotlib import animation
 import numpy as np
@@ -7,11 +7,77 @@ import random, time, spidev
 spi=spidev.SpiDev()
 spi.open(0, 0)
 spi.max_speed_hz=1000000
+ser = serial.Serial('/dev/ttyUSB0', 115200)
+def ReadChannel():
+    line = ser.readline()
+    line = line.decode("ISO-8859-1")
+    words = line.split(",")    # Fields split
+    
+    if(-1 < words[0].find('*')) :
+        data_from=1     # sensor data
+        data_index=0
+        text = "ID:"+'*'
+        words[0]=words[0].replace('*','')
+        #print ("first:", text)
+    else :
+        if(-1 < words[0].find('-')) :
+            data_from=2  # rf_receiver data
+            data_index=1
+            text = "ID:"+words[0]
+            #print ("seconds:",text)
+        else :
+            data_from=0  # unknown format
 
-def ReadChannel(channel):
-    adc=spi.xfer2([1,(8+channel)<<4,0])
-    data=((adc[1]&3)<<8)+adc[2]
+
+    if(data_from!=0):
+        commoma = words[data_index].find('.') 
+        if(len(words[data_index][commoma:-1])==4): # �Ҽ��� 4�ڸ� �Ǻ�
+            data_format = 2  # quaternion
+        else :
+            data_format = 1 # euler
+
+
+        if(data_format==1): #euler
+            try:
+                roll = float(words[data_index])*grad2rad
+                pitch = float(words[data_index+1])*grad2rad
+                yaw = float(words[data_index+2])*grad2rad
+                acc_x = float(words[data_index+3])
+                acc_y = float(words[data_index+4])
+                acc_z = float(words[data_index+5])
+                #print(roll)
+            except:
+                print (".")
+        else: #(data_format==2)quaternion
+            try:
+                q0 = float(words[data_index])
+                q1 = float(words[data_index+1])
+                q2 = float(words[data_index+2])
+                q3 = float(words[data_index+3])
+                acc_x = float(words[data_index+4])
+                acc_y = float(words[data_index+5])
+                acc_z = float(words[data_index+6])
+                Euler = quat_to_euler(q0,q1,q2,q3)
+
+                roll  = Euler[1]
+                pitch = Euler[0]
+                yaw   = Euler[2]
+            except:
+                print (".")
+        
+        text = words[0][-1:]
+        data = [text, roll, pitch, yaw,acc_x, acc_y, acc_z]
     return data
+
+
+
+
+
+
+
+
+
+
 mcp3008_channel=0
 fig = plt.figure()    
 ax = plt.subplot(211, xlim=(0, 50), ylim=(0, 1024))
@@ -26,7 +92,7 @@ line_2, = ax_2.plot(np.arange(max_points_2),
                 np.ones(max_points, dtype=np.float)*np.nan, lw=1,ms=1)
 
 def animate(i):
-    y = ReadChannel(mcp3008_channel)
+    y = ReadChannel()
     # y = random.randint(0,1000)
     old_y = line.get_ydata()
     new_y = np.r_[old_y[1:], y]
