df087a99ecf541db5b896527a3b0a47dc99f1938
이도현 <idohyeon@idohyeon-ui-MacBookPro.local>
2022-11-11 12:43:35 +0900

    simple_push

--- serial_data_real.py
+++ serial_data_real.py
@@ -39,65 +39,68 @@ def quat_to_euler(x,y,z,w):
     return euler
 
 def serial_read():
-
-    line = ser.readline()
-    line = line.decode("ISO-8859-1")
-    words = line.split(",")    # Fields split
+    if (count == 0):
+        count += 1
+        line = ser.readline()
+        line = line.decode("ISO-8859-1")
+        words = line.split(",")    # Fields split
     
-    if(-1 < words[0].find('*')) :
-        data_from=1     # sensor data
-        data_index=0
-        text = "ID:"+'*'
-        words[0]=words[0].replace('*','')
-        #print ("first:", text)
-    else :
-        if(-1 < words[0].find('-')) :
-            data_from=2  # rf_receiver data
-            data_index=1
-            text = "ID:"+words[0]
-            #print ("seconds:",text)
+        if(-1 < words[0].find('*')) :
+            data_from=1     # sensor data
+            data_index=0
+            text = "ID:"+'*'
+            words[0]=words[0].replace('*','')
+            #print ("first:", text)
         else :
-            data_from=0  # unknown format
+            if(-1 < words[0].find('-')) :
+                data_from=2  # rf_receiver data
+                data_index=1
+                text = "ID:"+words[0]
+                #print ("seconds:",text)
+            else :
+                data_from=0  # unknown format
 
 
-    if(data_from!=0):
-        commoma = words[data_index].find('.') 
-        if(len(words[data_index][commoma:-1])==4): # �Ҽ��� 4�ڸ� �Ǻ�
-            data_format = 2  # quaternion
-        else :
-            data_format = 1 # euler
+        if(data_from!=0):
+            commoma = words[data_index].find('.') 
+            if(len(words[data_index][commoma:-1])==4): # �Ҽ��� 4�ڸ� �Ǻ�
+                data_format = 2  # quaternion
+            else :
+                data_format = 1 # euler
 
 
-        if(data_format==1): #euler
-            try:
-                roll = float(words[data_index])*grad2rad
-                pitch = float(words[data_index+1])*grad2rad
-                yaw = float(words[data_index+2])*grad2rad
-                acc_x = float(words[data_index+3])
-                acc_y = float(words[data_index+4])
-                acc_z = float(words[data_index+5])
-                #print(roll)
-            except:
-                print (".")
-        else: #(data_format==2)quaternion
-            try:
-                q0 = float(words[data_index])
-                q1 = float(words[data_index+1])
-                q2 = float(words[data_index+2])
-                q3 = float(words[data_index+3])
-                acc_x = float(words[data_index+4])
-                acc_y = float(words[data_index+5])
-                acc_z = float(words[data_index+6])
-                Euler = quat_to_euler(q0,q1,q2,q3)
+            if(data_format==1): #euler
+                try:
+                    roll = float(words[data_index])*grad2rad
+                    pitch = float(words[data_index+1])*grad2rad
+                    yaw = float(words[data_index+2])*grad2rad
+                    acc_x = float(words[data_index+3])
+                    acc_y = float(words[data_index+4])
+                    acc_z = float(words[data_index+5])
+                    #print(roll)
+                except:
+                    print (".")
+            else: #(data_format==2)quaternion
+                try:
+                    q0 = float(words[data_index])
+                    q1 = float(words[data_index+1])
+                    q2 = float(words[data_index+2])
+                    q3 = float(words[data_index+3])
+                    acc_x = float(words[data_index+4])
+                    acc_y = float(words[data_index+5])
+                    acc_z = float(words[data_index+6])
+                    Euler = quat_to_euler(q0,q1,q2,q3)
 
-                roll  = Euler[1]
-                pitch = Euler[0]
-                yaw   = Euler[2]
-            except:
-                print (".")
-    #text = words[0][-1:]
-        data = save_data(roll, pitch, yaw,acc_x, acc_y, acc_z)
-        return data
+                    roll  = Euler[1]
+                    pitch = Euler[0]
+                    yaw   = Euler[2]
+                except:
+                    print (".")
+        #text = words[0][-1:]
+            data = save_data(roll, pitch, yaw,acc_x, acc_y, acc_z)
+            if (count == 6):
+                count = 0
+            return data
    
 
 
