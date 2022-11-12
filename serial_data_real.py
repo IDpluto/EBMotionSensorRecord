f7a99cdc26a032affdca7ae5c5ab8ac5c0ad8eb5
이도현 <idohyeon@idohyeon-ui-MacBookPro.local>
2022-11-12 16:02:17 +0900

    simple_push

--- serial_data_real.py
+++ serial_data_real.py
@@ -11,7 +11,7 @@ import time
 def animate(i):
     
     serial_read()
-    y = float(roll_s.popleft())
+    y = float(roll_s.pop())
     print(y)
     old_y = line.get_ydata()
     new_y = np.r_[old_y[1:], y]
@@ -22,7 +22,7 @@ def animate(i):
     
 def animate_2(i):
     serial_read()
-    y_2 = float(pitch_s.popleft())
+    y_2 = float(pitch_s.pop())
     old_y_2 = line_2.get_ydata()
     new_y_2 = np.r_[old_y_2[1:], y_2]
     line_2.set_ydata(new_y_2)
@@ -31,7 +31,7 @@ def animate_2(i):
 
 def animate_3(i):
     serial_read()
-    y_3 = float(yaw_s.popleft())
+    y_3 = float(yaw_s.pop())
     old_y_3= line_3.get_ydata()
     new_y_3 = np.r_[old_y_3[1:], y_3]
     line_3.set_ydata(new_y_3)
@@ -49,7 +49,7 @@ def animate_4(i):
 
 def animate_5(i):
     serial_read()
-    y_5 = float(ay_s.popleft())
+    y_5 = float(ay_s.pop())
     old_y_5= line_5.get_ydata(4)
     new_y_5 = np.r_[old_y_5[1:], y_5]
     line_5.set_ydata(new_y_5)
@@ -58,7 +58,7 @@ def animate_5(i):
 
 def animate_6(i):
     serial_read()
-    y_6 = float(az_s.popleft())
+    y_6 = float(az_s.pop())
     old_y_6= line_6.get_ydata()
     new_y_6 = np.r_[old_y_6[1:], y_6]
     line_6.set_ydata(new_y_6)
