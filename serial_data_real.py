c0ea8512424728c612dc8f3054de7b7b64c96462
이도현 <idohyeon@idohyeon-ui-MacBookPro.local>
2022-11-12 20:53:41 +0900

    simple_push

--- serial_data_real.py
+++ serial_data_real.py
@@ -327,9 +327,9 @@ if __name__ == '__main__':
 
     
     line, = ax.plot(np.arange(max_points), 
-        np.ones(max_points, dtype=np.float64)*np.nan, 'o-', lw=1, ms=1, c='blue',ms=1)
+        np.ones(max_points, dtype=np.float64)*np.nan, 'o-', lw=1, c='blue',ms=1)
     line_2, = ax.plot(np.arange(max_points), 
-        np.ones(max_points, dtype=np.float64)*np.nan, 'o-', lw=1, ms=1, c='green',ms=1)
+        np.ones(max_points, dtype=np.float64)*np.nan, 'o-', lw=1, c='green',ms=1)
     line_3, = ax.plot(np.arange(max_points), 
         np.ones(max_points, dtype=np.float64)*np.nan, 'o-', lw=1, ms=1, c='red',ms=1)
     line_4, = ax.plot(np.arange(max_points), 
