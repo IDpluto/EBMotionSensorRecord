30dff8cceb9b1e342e2c861f2aca547d71eadc78
이도현 <idohyeon@idohyeon-ui-MacBookPro.local>
2022-11-12 17:46:01 +0900

    simple_push

--- serial_data_real.py
+++ serial_data_real.py
@@ -251,7 +251,9 @@ if __name__ == '__main__':
     
 
     fig = plt.figure()    
-    ax = plt.subplot(111, xlim=(0, 40), ylim=(-700, 700))
+    ax = plt.subplot(211, xlim=(0, 40), ylim=(-700, 700))
+
+    ax.set_ylabel("Test")
     #ax = plt.title("test")
     ax_2 = plt.subplot(212, xlim=(0, 40), ylim=(-700, 700))
 
