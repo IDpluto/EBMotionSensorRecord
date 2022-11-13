6479c7905a9474e428133e3723195717178b3820
이도현 <idohyeon@idohyeon-ui-MacBookPro.local>
2022-11-13 20:09:38 +0900

    simple_push

--- serial_data_real.py
+++ serial_data_real.py
@@ -212,7 +212,7 @@ def save_data_head(roll, pitch, yaw):
 
 def save_csv():
     #day_c = day_p.pop()
-    time_c = time_p.pop()
+    time_c = str(time_p.pop())
     print (time_c)
     flag_gh1 = int(flag_ghand.pop())
     flag_ah1 = int(flag_ahand.pop())
