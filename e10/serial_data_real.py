923bdde67f537ab28fe3fa39e90943d54d5898ba
이도현 <idohyeon@idohyeon-ui-MacBookPro.local>
2022-11-30 21:48:39 +0900

    simple_push

--- e10/serial_data_real.py
+++ e10/serial_data_real.py
@@ -315,19 +315,19 @@ if __name__ == '__main__':
     
 
     fig = plt.figure()
-    ax = plt.subplot(211, xlim=(0, 3), ylim=(-300, 300))
+    ax = plt.subplot(211, xlim=(0, 1), ylim=(-300, 300))
     
     ax.set_title("hand")
     ax.set_ylabel("val")
     #ax = plt.title("test")
-    ax_2 = plt.subplot(212, xlim=(0, 3), ylim=(-300, 300))
+    ax_2 = plt.subplot(212, xlim=(0, 1), ylim=(-300, 300))
     ax_2.set_title("head")
     ax_2.set_ylabel("val")
     plt.tight_layout()
 
 
-    max_points = 4
-    max_points_2 = 4
+    max_points = 2
+    max_points_2 = 2
     count = 0
     fieldnames = ["Y-M-D", "H-M-S", "Roll_hand", "Pitch_hand", "Yaw_hand","Acc_x_hand", "Acc_y_hand", "Acc_z_hand",  "Roll_head", "Pitch_head",  "Yaw_head",  "Acc_x_head", "Acc_y_head", "Acc_z_head"]
     ser.write(b"<sor100>")
