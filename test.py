1ffedc1687bde1551968db5d243c1b91ec6b4f6a
이도현 <idohyeon@idohyeon-ui-MacBookPro.local>
2022-12-19 10:28:16 +0900

    simple_push

--- test.py
+++ test.py
@@ -397,9 +397,9 @@ if __name__ == '__main__':
     s_count = 0
     fieldnames = ["Y-M-D", "H-M-S", "Roll_hand", "Pitch_hand", "Yaw_hand","Acc_x_hand", "Acc_y_hand", "Acc_z_hand",  "Roll_head", "Pitch_head",  "Yaw_head",  "Acc_x_head", "Acc_y_head", "Acc_z_head"]
     ser.write(b"<sor100>")
-    time.sleep(10)
+    time.sleep(5)
     ser.write(b"<??cg>")
-    time.sleep(10)
+    time.sleep(5)
     
     
     with open('/home/dohlee/crc_project/data/data1.csv','w') as csv_file:
